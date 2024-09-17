764257171bb00501e83c9e45dd016fedf2cbf414
Charles Huffman <huffmancharles14@gmail.com>
2024-09-17 05:26:54 -0500

    add leafnode class and start writing tests to complete lesson

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,5 +19,25 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f'HTMLNode(tag={self.tag!r}, value={self.value!r}, children={self.children!r}, props={self.props!r})'
+    
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        if value is None:
+            raise ValueError("A value must be provided")
+        props = {}
+        super().__init__(tag=tag, value=value, children=None, props=props if props is not None else {})
 
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("A value must be provided for a LeafNode.")
+        
+        if self.tag is None:
+            return self.value
+        
+        props_string = ' '.join(f"'{key}={value}'" for key, value in self.props.items())
+        if props_string:
+            props_string = ' ' + props_string
 
+        return f'<{self.tag}{props_string}>{self.value}</{self.tag}>'
+
+ 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -17,6 +17,11 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode()
         with self.assertRaises(NotImplementedError):
             node.to_html()
+
+class TestLeafNode(unittest.TestCase):
+    def test_Leaf_to_html_without_tag(self):
+        node = LeafNode(value="This is a LeafNode")
+        self.assertEqual(node.to_html(), "This is a LeafNode")
 
 if __name__ == "__main__":
     unittest.main()
